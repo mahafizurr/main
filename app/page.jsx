ab6b3a0803cfe48b405b0adb93a9d979c432c355
mahafizurr <mahafizurgub@gmail.com>
2023-12-21 17:31:27 +0600

    five

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Nav from "./components/Nav";
 
 export default function Home() {
   return (
     <div>
+      <Nav />
       <h1 className="text-center font-bold text-4xl p-8 m-8">
         Good Evening, Biplob
       </h1>
